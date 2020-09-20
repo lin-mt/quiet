f82ff18e362668849d02a572c5923c15fff01468
lin-mt <lin-mt@outlook.com>
2020-09-21 07:42:55 +0800

    添加 query dsl 生成的文件

---
+++ quite-system/src/main/generated/com/gitee/quite/base/QBaseEntity.java
@@ -0,0 +1,45 @@
+package com.gitee.quite.base;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.Generated;
+import com.querydsl.core.types.Path;
+
+
+/**
+ * QBaseEntity is a Querydsl query type for BaseEntity
+ */
+@Generated("com.querydsl.codegen.SupertypeSerializer")
+public class QBaseEntity extends EntityPathBase<BaseEntity> {
+
+    private static final long serialVersionUID = 536008592L;
+
+    public static final QBaseEntity baseEntity = new QBaseEntity("baseEntity");
+
+    public final NumberPath<Long> creator = createNumber("creator", Long.class);
+
+    public final DateTimePath<java.time.LocalDateTime> gmtCreate = createDateTime("gmtCreate", java.time.LocalDateTime.class);
+
+    public final DateTimePath<java.time.LocalDateTime> gmtUpdate = createDateTime("gmtUpdate", java.time.LocalDateTime.class);
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    public final NumberPath<Long> updater = createNumber("updater", Long.class);
+
+    public QBaseEntity(String variable) {
+        super(BaseEntity.class, forVariable(variable));
+    }
+
+    public QBaseEntity(Path<? extends BaseEntity> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QBaseEntity(PathMetadata metadata) {
+        super(BaseEntity.class, metadata);
+    }
+
+}
+
