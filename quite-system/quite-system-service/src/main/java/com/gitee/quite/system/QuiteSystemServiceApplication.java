849cdebf211168b944a68dcd929b0b41647ec4d8
lin-mt <lin-mt@outlook.com>
2020-10-11 09:52:15 +0800

    拆分 quite-system

--- quite-system/quite-system-service/src/main/java/com/gitee/quite/system/QuiteSystemServiceApplication.java
+++ quite-system/quite-system-service/src/main/java/com/gitee/quite/system/QuiteSystemServiceApplication.java
@@ -4,13 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
+ * 启动类.
+ *
  * @author <a href="mailto:lin-mt@outlook.com">lin-mt</a>
  */
 @SpringBootApplication
-public class QuiteSystemApplication {
+public class QuiteSystemServiceApplication {
     
     public static void main(String[] args) {
-        SpringApplication.run(QuiteSystemApplication.class, args);
+        SpringApplication.run(QuiteSystemServiceApplication.class, args);
     }
     
 }
