a625823c4728439f86e634f6214039b441e5c674
lin-mt <lin-mt@outlook.com>
2021-02-23 20:40:05 +0800

    add copyright.

--- quiet-scrum/quiet-scrum-api/src/main/java/com/gitee/quiet/scrum/entity/ScrumProject.java
+++ quiet-scrum/quiet-scrum-api/src/main/java/com/gitee/quiet/scrum/entity/ScrumProject.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2021 lin-mt@outlook.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.gitee.quiet.scrum.entity;
 
 import com.gitee.quiet.common.service.base.BaseEntity;
@@ -19,12 +35,13 @@ import javax.validation.constraints.NotNull;
 @Entity
 @Table(name = "scrum_project")
 public class ScrumProject extends BaseEntity {
-
+    
     @NotNull(message = "{project.name}{not.null}")
     @Length(max = 30, message = "{project.name.length}{length.max.limit}")
     @Column(name = "project_name", nullable = false, length = 30)
     private String name;
     
+    @NotNull(message = "{project.manager}{not.null}")
     @Column(name = "manager", nullable = false)
     private Long manager;
     
@@ -42,7 +59,7 @@ public class ScrumProject extends BaseEntity {
     
     @Column(name = "task_template_id")
     private Long taskTemplateId;
-
+    
     @Enumerated(EnumType.STRING)
     @Column(name = "build_tool", length = 6)
     private BuildTool buildTool;
