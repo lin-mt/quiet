63381eb4c89a202db2fed653ed83e2ec7a965404
lin-mt <lin-mt@outlook.com>
2021-03-05 07:59:28 +0800

    basePackageClasses instead of basePackages.

--- quiet-scrum/quiet-scrum-service/src/main/java/com/gitee/quiet/scrum/QuietScrumServiceApplication.java
+++ quiet-scrum/quiet-scrum-service/src/main/java/com/gitee/quiet/scrum/QuietScrumServiceApplication.java
@@ -16,6 +16,7 @@
 
 package com.gitee.quiet.scrum;
 
+import com.gitee.quiet.scrum.repository.Scrum;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
@@ -26,7 +27,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
  *
  * @author <a href="mailto:lin-mt@outlook.com">lin-mt</a>
  */
-@EnableJpaRepositories(basePackages = "com.gitee.quiet.scrum.repository")
+@EnableJpaRepositories(basePackageClasses = Scrum.class)
 @SpringBootApplication(exclude = UserDetailsServiceAutoConfiguration.class)
 public class QuietScrumServiceApplication {
     
