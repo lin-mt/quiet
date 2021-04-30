d9d483fcb54751a31c857fe5b145df62ecb1afd7
lin-mt <lin-mt@outlook.com>
2021-04-30 12:26:54 +0800

    rename TitleKey to FrontSelect.

--- quiet-common/quiet-service/src/main/java/com/gitee/quiet/common/service/base/Parent.java
+++ quiet-common/quiet-service/src/main/java/com/gitee/quiet/common/service/base/Parent.java
@@ -27,7 +27,7 @@ import java.util.List;
  *
  * @author <a href="mailto:lin-mt@outlook.com">lin-mt</a>
  */
-public interface Parent<T extends Parent<T>> extends TitleKey {
+public interface Parent<T extends Parent<T>> extends FrontSelect {
     
     /**
      * 获取父的ID
