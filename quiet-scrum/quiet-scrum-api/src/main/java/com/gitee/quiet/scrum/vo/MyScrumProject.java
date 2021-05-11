3acdc4790579060847b45998ffa22adea83c73b0
lin-mt <lin-mt@outlook.com>
2021-05-12 00:03:57 +0800

    MyScrumProject default value.

--- quiet-scrum/quiet-scrum-api/src/main/java/com/gitee/quiet/scrum/vo/MyScrumProject.java
+++ quiet-scrum/quiet-scrum-api/src/main/java/com/gitee/quiet/scrum/vo/MyScrumProject.java
@@ -30,12 +30,12 @@ public class MyScrumProject {
     /**
      * 作为项目经理负责的项目
      */
-    private List<ScrumProject> projectManaged;
+    private List<ScrumProject> projectManaged = List.of();
     
     /**
      * 参与的项目（非项目经理）
      */
-    private List<ScrumProject> projectInvolved;
+    private List<ScrumProject> projectInvolved = List.of();
     
     public List<ScrumProject> getProjectManaged() {
         return projectManaged;
