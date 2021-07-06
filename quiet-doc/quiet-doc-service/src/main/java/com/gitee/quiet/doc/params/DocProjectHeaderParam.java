60af55e9c43ff63e1f7abb3f93d3b184daa49572
lin-mt <lin-mt@outlook.com>
2021-07-06 22:22:20 +0800

    add param.

---
+++ quiet-doc/quiet-doc-service/src/main/java/com/gitee/quiet/doc/params/DocProjectHeaderParam.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2021 lin-mt@outlook.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.gitee.quiet.doc.params;
+
+import com.gitee.quiet.common.service.base.Param;
+import com.gitee.quiet.doc.entity.DocProjectHeader;
+
+/**
+ * 项目所有请求的请求头信息.
+ *
+ * @author <a href="mailto:lin-mt@outlook.com">lin-mt</a>
+ */
+public class DocProjectHeaderParam extends Param<DocProjectHeader, DocProjectHeader> {
+
+}
