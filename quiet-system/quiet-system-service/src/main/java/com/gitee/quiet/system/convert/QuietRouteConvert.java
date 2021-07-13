bb7345dea35c4c9380d7c933e3bb356c0f8c6eee
lin-mt <lin-mt@outlook.com>
2021-07-13 21:56:02 +0800

    refactor: reconstruction QuietRoute.

--- quiet-system/quiet-system-service/src/main/java/com/gitee/quiet/system/convert/QuietRouteConvert.java
+++ quiet-system/quiet-system-service/src/main/java/com/gitee/quiet/system/convert/QuietRouteConvert.java
@@ -14,15 +14,20 @@
  * limitations under the License.
  */
 
-package com.gitee.quiet.common.validation.group.param.curd.batch;
+package com.gitee.quiet.system.convert;
 
-import javax.validation.groups.Default;
+import com.gitee.quiet.system.dto.QuietRouteDto;
+import com.gitee.quiet.system.entity.QuietRoute;
+import org.mapstruct.Mapper;
 
 /**
- * 批量新增.
+ * 权限信息实体信息转换.
  *
  * @author <a href="mailto:lin-mt@outlook.com">lin-mt</a>
  */
-public interface CreateBatch extends Default {
+@Mapper
+public interface QuietRouteConvert {
+    
+    QuietRoute dtoToEntity(QuietRouteDto dto);
     
 }
