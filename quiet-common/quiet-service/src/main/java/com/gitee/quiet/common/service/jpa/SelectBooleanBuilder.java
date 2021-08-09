6070a05f4de0a811acac2eb3d3fe1a1de303010a
lin-mt <lin-mt@outlook.com>
2021-08-10 00:00:50 +0800

    feat: boolean build support notEmptyIn.

--- quiet-common/quiet-service/src/main/java/com/gitee/quiet/common/service/jpa/SelectBooleanBuilder.java
+++ quiet-common/quiet-service/src/main/java/com/gitee/quiet/common/service/jpa/SelectBooleanBuilder.java
@@ -21,11 +21,13 @@ import com.gitee.quiet.common.service.jpa.entity.QDictionary;
 import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.types.Predicate;
 import com.querydsl.core.types.dsl.*;
+import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
 
 import javax.annotation.Nullable;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
+import java.util.Collection;
 import java.util.function.Consumer;
 
 /**
@@ -34,85 +36,85 @@ import java.util.function.Consumer;
  * @author <a href="mailto:lin-mt@outlook.com">lin-mt</a>
  */
 public class SelectBooleanBuilder extends SelectBuilder<BooleanBuilder> {
-    
+
     private final BooleanBuilder builder;
-    
+
     public SelectBooleanBuilder() {
         this.builder = new BooleanBuilder();
     }
-    
+
     public SelectBooleanBuilder(BooleanBuilder builder) {
         this.builder = builder == null ? new BooleanBuilder() : builder;
     }
-    
+
     @Override
     public BooleanBuilder getPredicate() {
         return builder;
     }
-    
+
     public SelectBooleanBuilder and(@Nullable Predicate right) {
         builder.and(right);
         return this;
     }
-    
+
     public SelectBooleanBuilder andAnyOf(Predicate... args) {
         builder.andAnyOf(args);
         return this;
     }
-    
+
     public SelectBooleanBuilder andNot(Predicate right) {
         return and(right.not());
     }
-    
+
     public SelectBooleanBuilder or(@Nullable Predicate right) {
         builder.or(right);
         return this;
     }
-    
+
     public SelectBooleanBuilder orAllOf(Predicate... args) {
         builder.orAllOf(args);
         return this;
     }
-    
+
     public SelectBooleanBuilder orNot(Predicate right) {
         return or(right.not());
     }
-    
+
     public SelectBooleanBuilder notNullEq(Boolean param, BooleanPath path) {
         if (param != null) {
             builder.and(path.eq(param));
         }
         return this;
     }
-    
+
     public <T extends Number & Comparable<?>> SelectBooleanBuilder notNullEq(T param, NumberPath<T> path) {
         if (param != null) {
             builder.and(path.eq(param));
         }
         return this;
     }
-    
+
     public SelectBooleanBuilder notBlankEq(String param, StringPath path) {
         if (StringUtils.isNoneBlank(param)) {
             builder.and(path.eq(param));
         }
         return this;
     }
-    
+
     public SelectBooleanBuilder with(@NotNull Consumer<SelectBooleanBuilder> consumer) {
         if (consumer != null) {
             consumer.accept(this);
         }
         return this;
     }
-    
+
     public <T extends Enum<T>> SelectBooleanBuilder notNullEq(T param, EnumPath<T> path) {
         if (param != null) {
             builder.and(path.eq(param));
         }
         return this;
     }
-    
+
     public SelectBooleanBuilder notBlankContains(String param, StringPath path) {
         if (StringUtils.isNoneBlank(param)) {
             builder.and(path.contains(param));
@@ -138,6 +140,13 @@ public class SelectBooleanBuilder extends SelectBuilder<BooleanBuilder> {
     public SelectBooleanBuilder notNullAfter(LocalDateTime param, DateTimePath<LocalDateTime> path) {
         if (param != null) {
             path.after(param);
+        }
+        return this;
+    }
+
+    public SelectBooleanBuilder notEmptyIn(Collection<? extends Long> param, NumberPath<Long> path) {
+        if (CollectionUtils.isNotEmpty(param)) {
+            path.in(param);
         }
         return this;
     }
